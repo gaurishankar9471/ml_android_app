bdf2c6dbce19434f6ca44e18074a85b46c350dcb
gaurishankar9471 <gaurishankar9471@gmail.com>
2019-12-13 07:48:24 +0530

    face detection completed

--- app/src/main/java/gaurishankar/com/hifisoftlab/mlapp/RecognizeFace.java
+++ app/src/main/java/gaurishankar/com/hifisoftlab/mlapp/RecognizeFace.java
@@ -1,14 +1,53 @@
 package gaurishankar.com.hifisoftlab.mlapp;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.hardware.Camera;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.vision.L;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionPoint;
+import com.google.firebase.ml.vision.face.FirebaseVisionFace;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceContour;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceLandmark;
+
+import java.util.List;
 
 public class RecognizeFace extends AppCompatActivity {
 
-    private Camera mCamera;
-    private CameraPreview mPreview;
+//    private Camera mCamera;
+//    private CameraPreview mPreview;
+
+
+
+    private Button mCameraBtn;
+    private Button mOpenGalleryBtn;
+    private final static int RESULT_LOAD_IMAGE = 1;
+    private ImageView mImgView;
+
 
 
     @Override
@@ -16,21 +55,156 @@ public class RecognizeFace extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recognize_face);
 
-        // Create an instance of Camera
-        mCamera = getCameraInstance();
+        mCameraBtn = findViewById(R.id.open_camera_btn);
+        mOpenGalleryBtn = findViewById(R.id.choose_gallery_btn);
 
-        // Create our Preview view and set it as the content of our activity.
-        mPreview = new CameraPreview(this, mCamera);
-        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-        preview.addView(mPreview);
+        mImgView = findViewById(R.id.imgview);
+
+
+        mCameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Opening Camera...", Toast.LENGTH_LONG);
+            }
+        });
+
+        mOpenGalleryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Opening Gallery", Toast.LENGTH_LONG).show();
+                //open gallery
+                Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+
+
     }
-    protected Camera getCameraInstance(){
-        Camera c = null;
-        try {
-            c = Camera.open();
-        } catch (Exception e){
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+
+            Bitmap bitmap = BitmapFactory.decodeFile(picturePath);
+            FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+
+//            mImg.setImageBitmap(bitmap);
+
+            processImage(image, bitmap);
+
+            cursor.close();
+//            ImageView imageView = (ImageView) findViewById(R.id.imgView);
+//            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
         }
-        return c;
+//        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK)
+//        {
+//            Bitmap photo = (Bitmap) data.getExtras().get("data");
+//            Bitmap b = Bitmap.createScaledBitmap(photo, 1200, 600, false);
+//            mImg.setImageBitmap(b);
+//
+//            FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(b);
+//            processImage(image,b);
+//        }
+    }
+
+    private void processImage(FirebaseVisionImage image, final Bitmap bitmap) {
+        FirebaseVisionFaceDetectorOptions highAccuracyOpts =
+                new FirebaseVisionFaceDetectorOptions.Builder()
+                        .setPerformanceMode(FirebaseVisionFaceDetectorOptions.ACCURATE)
+                        .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+                        .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+                        .build();
+        FirebaseVisionFaceDetector detector = FirebaseVision.getInstance()
+                .getVisionFaceDetector(highAccuracyOpts);
+
+
+        Task<List<FirebaseVisionFace>> result =
+                detector.detectInImage(image)
+                        .addOnSuccessListener(
+                                new OnSuccessListener<List<FirebaseVisionFace>>() {
+                                    @Override
+                                    public void onSuccess(List<FirebaseVisionFace> faces) {
+                                        // Task completed successfully
+                                        // ...
+                                        Toast.makeText(getApplicationContext(),"Success", Toast.LENGTH_LONG).show();
+                                        Bitmap mutableBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+
+
+                                        for (FirebaseVisionFace face : faces) {
+                                            Rect bounds = face.getBoundingBox();
+
+                                            //draw rect bar start
+
+
+
+                                            Canvas canvas = new Canvas(mutableBitmap);
+
+                                            // Draw a solid color to the canvas background
+
+                                            // Initialize a new Paint instance to draw the Rectangle
+                                            Paint paint = new Paint();
+                                            paint.setColor(Color.RED);
+                                            paint.setStrokeWidth(5);
+                                            paint.setStyle(Paint.Style.STROKE);
+
+
+                                            canvas.drawRect(bounds,paint);
+
+
+                                            //draw rect bar end
+
+                                            float rotY = face.getHeadEulerAngleY();  // Head is rotated to the right rotY degrees
+                                            float rotZ = face.getHeadEulerAngleZ();  // Head is tilted sideways rotZ degrees
+
+                                            // If landmark detection was enabled (mouth, ears, eyes, cheeks, and
+                                            // nose available):
+                                            FirebaseVisionFaceLandmark leftEar = face.getLandmark(FirebaseVisionFaceLandmark.LEFT_EAR);
+                                            if (leftEar != null) {
+                                                FirebaseVisionPoint leftEarPos = leftEar.getPosition();
+                                            }
+
+                                            // If contour detection was enabled:
+                                            List<FirebaseVisionPoint> leftEyeContour =
+                                                    face.getContour(FirebaseVisionFaceContour.LEFT_EYE).getPoints();
+                                            List<FirebaseVisionPoint> upperLipBottomContour =
+                                                    face.getContour(FirebaseVisionFaceContour.UPPER_LIP_BOTTOM).getPoints();
+
+                                            // If classification was enabled:
+                                            if (face.getSmilingProbability() != FirebaseVisionFace.UNCOMPUTED_PROBABILITY) {
+                                                float smileProb = face.getSmilingProbability();
+                                            }
+                                            if (face.getRightEyeOpenProbability() != FirebaseVisionFace.UNCOMPUTED_PROBABILITY) {
+                                                float rightEyeOpenProb = face.getRightEyeOpenProbability();
+                                            }
+
+                                            // If face tracking was enabled:
+                                            if (face.getTrackingId() != FirebaseVisionFace.INVALID_ID) {
+                                                int id = face.getTrackingId();
+                                            }
+                                        }
+                                        mImgView.setImageBitmap(mutableBitmap);
+
+
+                                    }
+                                })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        // Task failed with an exception
+                                        // ...
+                                        Toast.makeText(getApplicationContext(),"Failed", Toast.LENGTH_LONG).show();
+                                    }
+                                });
+
     }
 
 }
